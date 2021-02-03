be73aa739661a406f2431f69734ba4da951bd168
nicotitine <nico.hincelin@gmail.com>
2021-02-03 17:40:39 +0100

    1 file modified, 5 added, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { BrowserModule } from "@angular/platform-browser";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
 import { EventsListComponent } from "./events/events-list.component";
 import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    EventsListComponent,
-    EventThumbnailComponent
-  ],
+  declarations: [AppComponent, EventsListComponent, EventThumbnailComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
